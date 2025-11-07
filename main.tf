17421ab629624b6e4b1d8fe2b9b4a6f6f6809803
Pavithra2294 <pav.ganesan@gmail.com>
2025-11-07 18:11:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,26 @@
-provider "aws" {
-  region = "eu-north-1"
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "pavithra-terraform-demo-bucket2"
-}
-
-
 # S3 Bucket for Source Data
-resource "aws_s3_bucket" "source-data-bucket" {
-  bucket        = "source-data-bucket"
+resource "aws_s3_bucket" "source-data-bucket-pre" {
+  bucket        = "source-data-bucket-pre"
   force_destroy = true
 }
 
 
 # S3 Bucket for Traget 
-resource "aws_s3_bucket" "target-data-bucket" {
-  bucket        = "target-data-bucket"
+resource "aws_s3_bucket" "finance-data-target-pre" {
+  bucket        = "finance-data-target-pre"
   force_destroy = true
 }
 
-
-resource "aws_s3_bucket" "code_bucket_paithra11" {
-  bucket = "code-bucket-paithra1"   # real AWS bucket name (hyphens OK here)
+resource "aws_s3_bucket" "glue_code_bucket_pre" {
+  bucket = "glue_code_bucket_pre"   # real AWS bucket name (hyphens OK here)
   acl    = "private"
 }
 
 
-resource "aws_s3_object" "script_file11" {
-  bucket = aws_s3_bucket.code_bucket_paithra11.id
+resource "aws_s3_object" "glue_script_file" {
+  bucket = aws_s3_bucket.glue_code_bucket_pre.id
   key    = "script.py"           
-  source = "script.py"  
+  source = "C:/Users/ashok/OneDrive/Desktop/terraform/script.py"  
 }
-
 
 
